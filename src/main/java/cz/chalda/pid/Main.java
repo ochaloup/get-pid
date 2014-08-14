d74d526a3c494fa432ec8a6f50debb20e7774cb3
Ondrej Chaloupka <ochaloup@redhat.com>
2014-08-14 13:35:36 +0200

    Fixing test command for windows

--- src/main/java/cz/chalda/pid/Main.java
+++ src/main/java/cz/chalda/pid/Main.java
@@ -22,7 +22,7 @@ public class Main {
       process = starter.start(command);
       System.out.println("Pid of the underlined process is: " + GetPid.getUnixPid(process));
     } else {
-      String[] command = new String[] {"echo", "\"A\""};
+      String[] command = new String[] {"cmd", "/C", "for %I in (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21) DO echo Sleeping... & timeout /T 10"};
       process = starter.start(command);
       System.out.println("Pid of the underlined process is: " + GetPid.getWindowsPid(process));
     }
